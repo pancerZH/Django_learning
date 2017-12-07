6fbc342173f46978f04f5833d948284aa571c8e9
DESKTOP-DLOBL4I\张文喆 <942740938@qq.com>
2017-12-07 21:38:33 +0800

    增添404提示

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
+from django.shortcuts import render
 from .models import Question
 
 # Create your views here.
@@ -12,7 +13,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse('You\'re looking at question {}.'.format(question_id))
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist!')
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
