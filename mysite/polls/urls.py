6d966b96642fb39def9921f2e69ba8bfe5fa0491
DESKTOP-DLOBL4I\张文喆 <942740938@qq.com>
2017-12-08 20:02:28 +0800

    修正笔误

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [url(r'^$', views.index, name='index'),
-               url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='details'),
+               url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
                url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
                url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
                ]
